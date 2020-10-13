ebc4d53427eeccb4ee538888843ac7b16d659906
antstu <anthony.stuvecke@gmail.com>
2020-10-13 23:31:55 +0200

    io

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -13,6 +15,34 @@ class Example {
     public static void main(String[] args) {
 
 
+        FileInputStream inputStream = null;
+        BufferedInputStream bufferedInputStream = null;
+
+        String filePath = "src/labs_examples/input_output/files/write_test.txt";
+
+        try (FileWriter fw = new FileWriter(filePath)) {
+//            TODO: use abs path ?
+            inputStream = new FileInputStream("src/labs_examples/input_output/files/char_data.txt");
+            bufferedInputStream = new BufferedInputStream(inputStream);
+
+            byte[] buffer = new byte[5];
+            int bytesRead = 0;
+
+            while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
+                fw.write(new String(buffer, 0, bytesRead));
+            }
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+                bufferedInputStream.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
 
     }
+
 }
+
