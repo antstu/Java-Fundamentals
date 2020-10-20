7379c9ff37369d0670b6858766eff08ffd2a4066
antstu <anthony.stuvecke@gmail.com>
2020-10-20 12:34:26 +0200

    generics

--- src/labs_examples/generics/labs/Exercise_02.java
+++ src/labs_examples/generics/labs/Exercise_02.java
@@ -1,8 +1,43 @@
 package labs_examples.generics.labs;
 
+import java.util.ArrayList;
+
 /**
  * Generics Exercise 2:
  *
  *      Create a class with a generic method that takes in an ArrayList of any Numeric type and returns the sum of all
  *      Numbers in the ArrayList
  */
+
+
+
+class Main2 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> array = new ArrayList();
+
+        array.add(1);
+        array.add(2);
+        array.add(3);
+
+        double total = Generic2.sum(array);
+
+        System.out.println(total);
+
+    }
+
+}
+
+
+class Generic2 {
+
+    public static <N extends Number> double sum(ArrayList<N> arr) {
+        double sum = 0;
+
+        for(N number :arr) {
+            sum += number.doubleValue();
+        }
+        return sum;
+    }
+}
