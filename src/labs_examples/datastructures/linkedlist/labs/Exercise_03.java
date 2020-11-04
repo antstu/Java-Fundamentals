16db9e9658d827f932e46c6168f5f70d73bc6e54
antstu <anthony.stuvecke@gmail.com>
2020-11-04 11:44:52 +0100

    linkedlists

--- src/labs_examples/datastructures/linkedlist/labs/Exercise_03.java
+++ src/labs_examples/datastructures/linkedlist/labs/Exercise_03.java
@@ -5,3 +5,34 @@ package labs_examples.datastructures.linkedlist.labs;
  *
  *      Please create your own custom doubly-LinkedList from start to finish :) You got this!
  */
+
+
+
+class Exercise_03 {
+
+    public Node head = null;
+    public Node end = null;
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public void insert(int data){
+
+        Node node = new Node(data);
+        Node end = new Node(data);
+
+        if(head == null) {
+            head = node;
+            node.next = head;
+        }
+        else {
+            end.next = node;
+            end = node;
+            end.next = head;
+        }
+    }
+
+
+}
