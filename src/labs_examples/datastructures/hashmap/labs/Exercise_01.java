a2437aa303c9056ccc81d0de8f70c9b63737abad
antstu <anthony.stuvecke@gmail.com>
2020-11-05 12:42:02 +0100

    hashmap

--- src/labs_examples/datastructures/hashmap/labs/Exercise_01.java
+++ src/labs_examples/datastructures/hashmap/labs/Exercise_01.java
@@ -1,5 +1,10 @@
 package labs_examples.datastructures.hashmap.labs;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 /**
  *  HashMaps Exercise_01
  *
@@ -21,3 +26,44 @@ package labs_examples.datastructures.hashmap.labs;
  *  clear()
  *
  */
+
+public class Exercise_01 {
+
+    public static void main(String[] args){
+
+        HashMap<String, Person> peopleMap = new HashMap();
+
+
+        // create a few Person objects
+        Person anthony = new Person("Anthony", "Stuvecke", "anthony@gmail.com");
+        Person phil = new Person("Phil", "Stuvecke", "phil@gmail.com");
+        Person tom = new Person("Tom", "Stuvecke", "tom@gmai.com");
+        Person bob = new Person("Bob", "Stuvecke", "bob@gmail.com");
+
+
+        // "put" the Person objects into the HashMap
+        peopleMap.put(ryan.getEmail(), ryan);
+        peopleMap.put(kim.getEmail(), kim);
+        peopleMap.put(martin.getEmail(), martin);
+        peopleMap.put(caden.getEmail(), caden);
+
+
+        // demonstrate "getting" an element out of the HashMap
+        Person example = peopleMap.get("caden@codingnomads.co");
+        System.out.println(example.toString());
+
+
+        // demonstrate iterating through the entries of a HashMap
+        Set entries = peopleMap.entrySet();
+        Iterator iterator = entries.iterator();
+        // loop through the entries
+        while(iterator.hasNext()) {
+            // get each Entry individually
+            Map.Entry person = (Map.Entry)iterator.next();
+            // print out the entry's key and value
+            System.out.print("The key is: "+ person.getKey()
+                    + " and the value is: " + person.getValue().toString());
+        }
+    }
+
+}
